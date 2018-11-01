7758d488cfe263b35a3cb15465247ab360bf5efd
Martin Cantor <andres.cantor-u@mail.escuelaing.edu.co>
2018-11-01 18:44:53 -0500

    Inicio Laboratorio 6

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -36,7 +36,7 @@ var app = (function () {
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/TOPICXX', function (eventbody) {
+            stompClient.subscribe('/topic/newpoint', function (eventbody) {
                 
                 
             });
@@ -59,6 +59,7 @@ var app = (function () {
             var pt=new Point(px,py);
             console.info("publishing point at "+pt);
             addPointToCanvas(pt);
+            stompClient.send("/topic/newpoint", {}, JSON.stringify(pts));
 
             //publicar el evento
         },
